f309803e29b7dc0d146acaa12b662add56b2ea8e
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-07-14 00:28:52 +0100

    adding retweets feature

--- twitter/tweets.go
+++ twitter/tweets.go
@@ -1,6 +1,7 @@
 package twitter
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -32,7 +33,7 @@ type TweetService struct {
 // newTweetService returns a new TweetService.
 func newTweetService(sling *sling.Sling) *TweetService {
 	return &TweetService{
-		sling: sling.Path(""),
+		sling: sling.Path("tweets"),
 	}
 }
 
@@ -42,17 +43,7 @@ type PostTweetParams struct {
 	ForSuperFollowersOnly 	bool 			`json:"for_super_followers_only,omitempty"`
 	QuoteTweetId			string 			`json:"quote_tweet_id,omitempty"`
 	Text 					string 			`json:"text,omitempty"`
-}
-
-type ReplySetting string
-
-const (
-    MentionedUsers ReplySetting = "mentionedUsers"
-    Following      ReplySetting = "following"
-)
-
-type TweetGeoObj struct {
-	PlaceId		string `json:"place_id,omitempty"`
+	Media					TweetMedia		`json:"media,omitempty"`
 }
 
 type TweetMedia struct {
@@ -60,16 +51,6 @@ type TweetMedia struct {
 	TaggedUserIds		[]string		`json:"tagged_user_ids,omitempty"`
 }
 
-type TweetPoll struct {
-	DurationMinutes		int			`json:"duration_minutes,omitempty"`
-	Options				[]string	`json:"options,omitempty"`
-
-}
-
-type TweetReply struct {
-	ExcludeReplyUserIds			[]string		`json:"exclude_reply_user_ids,omitempty"`
-	InReplyToTweetId			string 			`json:"in_reply_to_tweet_id,omitempty"`
-}
 
 // Post tweet makes a new tweet.
 // Requires a user auth context.
@@ -82,8 +63,79 @@ func (s *TweetService) PostTweet(text string, params *PostTweetParams) (*Tweet, 
 	tweet := new(Tweet)
 	apiError := new(APIError)
 
-	resp, err := s.sling.New().Post("tweets").BodyJSON(params).Receive(tweet, apiError)
+	ddJSON(params)
+
+	resp, err := s.sling.New().Post("").BodyJSON(params).Receive(tweet, apiError)
 	return tweet, resp, relevantError(err, *apiError)
+}
+
+// Destroy deletes the Tweet with the given id and returns it if successful.
+// Requires a user auth context.
+// https://developer.twitter.com/en/docs/twitter-api/tweets/manage-tweets/api-reference/delete-tweets-id
+func (s *TweetService) Destroy(id string) (*Tweet, *http.Response, error) {
+	tweet := new(Tweet)
+	apiError := new(APIError)
+	path := fmt.Sprintf("/%v", id)
+	resp, err := s.sling.New().Delete(path).Receive(tweet, apiError)
+
+	return tweet, resp, relevantError(err, *apiError)
+}
+
+// GetTweetsParams are the parameters for TweetService.Lookup
+type GetTweetsParams struct {
+	Ids		[]string 	`url:"ids,omitempty,comma"`
+}
+
+// Lookup returns the requested Tweets as a slice. Combines ids from the
+// required ids argument and from params.Id.
+// https://developer.twitter.com/en/docs/twitter-api/tweets/lookup/api-reference/get-tweets
+func (s *TweetService) GetTweets(ids []string, params *GetTweetsParams) ([]Tweet, *http.Response, error) {
+	if params == nil {
+		params = &GetTweetsParams{}
+	}
+	params.Ids = append(params.Ids, ids...)
+	tweets := new([]Tweet)
+	apiError := new(APIError)
+	resp, err := s.sling.New().Get("").QueryStruct(params).Receive(tweets, apiError)
+
+	return *tweets, resp, relevantError(err, *apiError)
+}
+
+// Show returns the requested Tweet.
+// https://developer.twitter.com/en/docs/twitter-api/tweets/lookup/api-reference/get-tweets-id
+func (s *TweetService) Show(id string) (*Tweet, *http.Response, error) {
+	tweet := new(Tweet)
+	apiError := new(APIError)
+	path := fmt.Sprintf("/%v", id)
+	resp, err := s.sling.New().Get(path).Receive(tweet, apiError)
+
+	return tweet, resp, relevantError(err, *apiError)
+}
+
+// StatusRetweeterParams are the parameters for StatusService.Retweeters.
+type TweetRetweeterParams struct {
+	MaxResults     int64 `url:"max_results,omitempty"`
+}
+
+// RetweeterIDs is a cursored collection of User IDs that retweeted a Tweet.
+type Retweeter struct {
+	ID					string 		`json:"id"`
+	Name				string		`json:"name"`
+	Username			string		`json:"username"`
+	CreatedAt			string 		`json:"created_at,omitempty"`
+	Description			string 		`json:"description,omitempty"`
+	PinnedTweetId		string 		`json:"pinned_tweet_id,omitempty"`
+}
+
+// Retweeters return the retweeters of a specific tweet.
+// https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/get-statuses-retweeters-ids
+func (s *TweetService) Retweeters(id string, params *TweetRetweeterParams) (*[]Retweeter, *http.Response, error) {
+	retweeters := new([]Retweeter)
+	apiError := new(APIError)
+	path := fmt.Sprintf("/%v/retweeted_by", id)
+	resp, err := s.sling.Get(path).QueryStruct(params).Receive(retweeters, apiError)
+
+	return retweeters, resp, relevantError(err, *apiError)
 }
 
 
